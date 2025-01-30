9dd99a9d02ae9432afb5f5dac14cbd30c72e9331
gootthedoot <gabrielgarayalde747@gmail.com>
2025-01-30 18:40:02 -0500

    Changed "Hello" to "Goodbye"

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Goodbye, World!");
     }
 
 }
