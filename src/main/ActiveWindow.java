9ec120489bfa9290d18481431874a850e9662d4a
gootthedoot <gabrielgarayalde747@gmail.com>
2025-02-03 11:21:30 -0500

    Moved construction of window to activeWindow class

--- src/main/ActiveWindow.java
+++ src/main/ActiveWindow.java
@@ -1,9 +1,25 @@
 package main;
 
-public class ActiveWindow{
+import java.awt.Component;
+import java.awt.Dimension;
+import javax.swing.JFrame;
 
+public class ActiveWindow{
+    public static JFrame window;
+    public static Component activeWindow= new Component() {
+
+    };
+    public static Dimension minSize;
     public ActiveWindow(){
-        
+        window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(true);
+        window.setMinimumSize(minSize);
+        window.setTitle("Programmer Professional");
+        window.add(activeWindow);
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,30 +1,12 @@
 package main;
 
-import java.awt.Component;
-import javax.swing.JFrame;
-
 public class Main {
-
-    public static JFrame window;
-    public static Component activeWindow= new Component() {
-        
-    };
 
     public static void main(String[] args) throws Exception {
         System.out.println("Running...");
-        setUpWindow();
+        ActiveWindow window= new ActiveWindow();
 
     }
 
     //Sets up the main window JFrame used throughout the program for now.
-    public static void setUpWindow(){
-        window = new JFrame();
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setResizable(true);
-        window.setTitle("Programmer Professional");
-        window.add(activeWindow);
-        window.pack();
-        window.setLocationRelativeTo(null);
-        window.setVisible(true);
-    }
 }
